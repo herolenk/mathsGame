b9d0ebd857326b9be28607ece149a42006d02a9d
Mustafa Katipoğlu <herolenk@hotmail.com>
2017-07-14 17:22:18 +0300

    Dividing Javascript to two files and Separatinng Sections by its Logic

---
+++ Javascript/Code1.js
@@ -0,0 +1,42 @@
+/*///////////////*/
+
+/* Area 1: Clicking on Start-Reset Button */ 
+
+/*///////////////*/
+
+
+
+/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+
+/* If we click on the start/reset button If we are playing this means button is reset button*/ 
+/* Therefore do this */ 
+/* Reload the page*/
+
+/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+
+
+
+
+
+
+
+
+
+/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+
+/* If we click on the start/reset button If we are not playing this means button is start button*/
+/* Therefore do this */
+/* Generate new question&answers */ 
+/* Change button test to reset game */ 
+/* Show countdown box */
+/* Reduce time by 1sec in loops */
+/* Every sec check if time left  */
+/* If time left, continue  */
+/* If time's up, game over  */
+
+/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+
+
+
+
+
