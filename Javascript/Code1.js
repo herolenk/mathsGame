d6f163ce9d7d8d4fb244f6b3b240017550d96183
Mustafa Katipoğlu <herolenk@hotmail.com>
2017-07-15 07:53:53 +0300

    Decreasing timeRemainingValue every second

--- Javascript/Code1.js
+++ Javascript/Code1.js
@@ -1,42 +1,67 @@
 /*///////////////*/
 
-/* Area 1: Clicking on Start-Reset Button */ 
+/* Area 1: Clicking on Start-Reset Button */
 
 /*///////////////*/
 
 
 
-/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+/*///////////////*/
+/*///////////////*/ /*///////////////*/ /*///////////////*/ /*///////////////*/
 
-/* If we click on the start/reset button If we are playing this means button is reset button*/ 
-/* Therefore do this */ 
+/* If we click on the start/reset button If we are playing this means button is reset button*/
+/* Therefore do this */
 /* Reload the page*/
 
-/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+/*///////////////*/
+/*///////////////*/ /*///////////////*/ /*///////////////*/ /*///////////////*/
+
+var remainingTime = 60;
+
+var timeRemainingValue = document.getElementById("timeRemainingValue");
+
+var checkTime = setInterval(decreaseTime, 1000);
+
+function decreaseTime(){
+    remainingTime--;
+    timeRemainingValue.innerHTML = remainingTime;
+}
 
 
 
 
 
+//
+//document.getElementById("start-reset").onclick = function () {
+//    if (timeRemainingValue > 0) {
+//        document.getElementById("start-reset").innerHTML = "Reset";
+//    }else{
+//        document.getElementById("start-reset").innerHTML = "Start Game";
+//    }
+//}
+//
+//
+//
+//
+//
 
 
 
 
-/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
+
+/*///////////////*/
+/*///////////////*/ /*///////////////*/ /*///////////////*/ /*///////////////*/
 
 /* If we click on the start/reset button If we are not playing this means button is start button*/
 /* Therefore do this */
-/* Generate new question&answers */ 
-/* Change button test to reset game */ 
+/* Set score to 0*/
+/* Generate new question&answers */
+/* Change button test to reset game */
 /* Show countdown box */
 /* Reduce time by 1sec in loops */
 /* Every sec check if time left  */
 /* If time left, continue  */
 /* If time's up, game over  */
 
-/*///////////////*//*///////////////*//*///////////////*//*///////////////*//*///////////////*/
-
-
-
-
-
+/*///////////////*/
+/*///////////////*/ /*///////////////*/ /*///////////////*/ /*///////////////*/
